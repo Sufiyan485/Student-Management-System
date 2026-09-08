a7cbe286d14036dbdf72c0dbd829af50f75f9f97
Mohammed Sufiyan <ms9703303@gmail.com>
2026-09-08 23:02:07 +0530

    add data structure for students and teachers with JSON handling

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from abc import ABC, abstractmethod
 import json
 
 class Person(ABC):
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+
     @abstractmethod
     def get_role(self):
         pass
@@ -10,13 +15,20 @@ class Person(ABC):
     def show_details(self):
         pass
 
+    def update_name(self, new_name):
+        self.name = new_name
+
+    def update_age(self, new_age):
+        self.age = new_age
+
+    def update_email(self, new_email):
+        self.email = new_email
+
 
 class Student(Person):
     def __init__(self, student_id, name, age, email, roll_no):
+        super().__init__(name, age, email)
         self.student_id = student_id
-        self.name = name
-        self.age = age
-        self.email = email
         self.roll_no = roll_no
         self.grades = {}
 
@@ -41,29 +53,17 @@ class Student(Person):
             "roll_no": self.roll_no,
             "grades": self.grades
         }
-
+    
     def add_grade(self, subject, grade):
         self.grades[subject] = grade
-
-    def update_name(self, new_name):
-        self.name = new_name
-
-    def update_age(self, new_age):
-        self.age = new_age
-
-    def update_email(self, new_email):
-        self.email = new_email
 
     def update_roll_no(self, new_roll_no):
         self.roll_no = new_roll_no
 
-
 class Teacher(Person):
     def __init__(self, emp_id, name, age, email, subject):
+        super().__init__(name, age, email)
         self.emp_id = emp_id
-        self.name = name
-        self.age = age
-        self.email = email
         self.subject = subject
 
     def get_role(self):
@@ -86,15 +86,6 @@ class Teacher(Person):
             "subject": self.subject
         }
 
-    def update_name(self, new_name):
-        self.name = new_name
-
-    def update_age(self, new_age):
-        self.age = new_age
-
-    def update_email(self, new_email):
-        self.email = new_email
-
     def update_subject(self, new_subject):
         self.subject = new_subject
 
@@ -106,7 +97,7 @@ class ManagementSystem:
         self.next_teacher_id = 1
 
     @staticmethod
-    def get_valid_int(prompt,min_value,max_value):
+    def get_valid_int(prompt, min_value, max_value):
         while True:
             try:
                 value = int(input(prompt))
@@ -134,61 +125,6 @@ class ManagementSystem:
                 return email
             else:
                 print("Please enter a valid email address.")
-
-    def get_students_data(self):
-        students_data = {}
-        for student_id, student in self.students.items():
-            students_data[student_id] = student.to_dict()
-        return students_data
-
-    def get_data(self):
-        data = {
-            "students": self.get_students_data(),
-            "teachers": self.get_teachers_data()
-        }
-        return data
-
-    def save_data_to_file(self, filename):
-        data = self.get_data()
-        with open(filename, 'w') as f:
-            json.dump(data, f, indent=4)
-
-    def load_students_data(self, data):
-        for student_id, student_info in data.items():
-            student = Student(
-                student_id=student_info["student_id"],
-                name=student_info["name"],
-                age=student_info["age"],
-                email=student_info["email"],
-                roll_no=student_info["roll_no"]
-            )
-            for subject, grade in student_info.get("grades", {}).items():
-                student.add_grade(subject, grade)
-            self.students[student_id] = student
-
-    def load_data_from_file(self, filename):
-        try:
-            with open(filename, 'r') as f:
-                data = json.load(f)
-                self.load_students_data(data.get("students", {}))
-                self.load_teachers_data(data.get("teachers", {}))
-
-                self.recalculate_next_ids()
-        except FileNotFoundError:
-            print("\nNo saved data found. Starting fresh!")
-
-    def recalculate_next_ids(self):
-            students_ids = []
-            for student_id in self.students.keys():
-                students_ids.append(int(student_id[3:]))
-            highest_student_id = max(students_ids) if students_ids else 0
-            self.next_student_id = highest_student_id + 1
-
-            teachers_ids = []
-            for emp_id in self.teachers.keys():
-                teachers_ids.append(int(emp_id[3:]))
-            highest_teacher_id = max(teachers_ids) if teachers_ids else 0
-            self.next_teacher_id = highest_teacher_id + 1
 
     def register_student(self):
         student_id = f"STD{self.next_student_id:03d}"
@@ -218,7 +154,7 @@ class ManagementSystem:
 
         if std_id in self.students:
             subject = ManagementSystem.get_valid_text("Subject :- ").capitalize()
-            grade = ManagementSystem.get_valid_int("Grade (0-100) :- ", 0,100)
+            grade = ManagementSystem.get_valid_int("Grade (0-100) :- ", 0, 100)
             student = self.students[std_id]
             student.add_grade(subject, grade)
             print("Grade added successfully!")
@@ -289,23 +225,6 @@ class ManagementSystem:
         else:
             print("Student Not Found!")
 
-    def get_teachers_data(self):
-        teachers_data = {}
-        for emp_id, teacher in self.teachers.items():
-            teachers_data[emp_id] = teacher.to_dict()
-        return teachers_data
-
-    def load_teachers_data(self, data):
-        for emp_id, teacher_info in data.items():
-            teacher = Teacher(
-                emp_id=teacher_info["emp_id"],
-                name=teacher_info["name"],
-                age=teacher_info["age"],
-                email=teacher_info["email"],
-                subject=teacher_info["subject"]
-            )
-            self.teachers[emp_id] = teacher
-
     def register_teacher(self):
         emp_id = f"EMP{self.next_teacher_id:03d}"
         name = ManagementSystem.get_valid_text("Enter your name :- ")
@@ -352,7 +271,7 @@ class ManagementSystem:
                     print("Name updated successfully!")
 
                 elif choice == 2:
-                    new_age =  ManagementSystem.get_valid_int("Enter new age :- ", 5, 100)
+                    new_age = ManagementSystem.get_valid_int("Enter new age :- ", 5, 100)
                     teacher.update_age(new_age)
                     print("Age updated successfully!")
 
@@ -392,6 +311,80 @@ class ManagementSystem:
                     continue
         else:
             print("Teacher not found!")
+    
+    def get_students_data(self):
+        students_data = {}
+        for student_id, student in self.students.items():
+            students_data[student_id] = student.to_dict()
+        return students_data
+
+    def get_teachers_data(self):
+        teachers_data = {}
+        for emp_id, teacher in self.teachers.items():
+            teachers_data[emp_id] = teacher.to_dict()
+        return teachers_data
+
+    def get_data(self):
+        data = {
+            "students": self.get_students_data(),
+            "teachers": self.get_teachers_data()
+        }
+        return data
+
+    def save_data_to_file(self, filename):
+        data = self.get_data()
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
+
+    def load_students_data(self, data):
+        for student_id, student_info in data.items():
+            student = Student(
+                student_id=student_info["student_id"],
+                name=student_info["name"],
+                age=student_info["age"],
+                email=student_info["email"],
+                roll_no=student_info["roll_no"]
+            )
+            for subject, grade in student_info.get("grades", {}).items():
+                student.add_grade(subject, grade)
+            self.students[student_id] = student
+
+    def load_teachers_data(self, data):
+        for emp_id, teacher_info in data.items():
+            teacher = Teacher(
+                emp_id=teacher_info["emp_id"],
+                name=teacher_info["name"],
+                age=teacher_info["age"],
+                email=teacher_info["email"],
+                subject=teacher_info["subject"]
+            )
+            self.teachers[emp_id] = teacher
+
+    def load_data_from_file(self, filename):
+        try:
+            with open(filename, 'r') as f:
+                data = json.load(f)
+                self.load_students_data(data.get("students", {}))
+                self.load_teachers_data(data.get("teachers", {}))
+
+                self.recalculate_next_ids()
+        except FileNotFoundError:
+            print("\nNo saved data found. Starting fresh!")
+        except json.JSONDecodeError:
+            print("\nError reading data file. Starting fresh!")
+
+    def recalculate_next_ids(self):
+        students_ids = []
+        for student_id in self.students.keys():
+            students_ids.append(int(student_id[3:]))
+        highest_student_id = max(students_ids) if students_ids else 0
+        self.next_student_id = highest_student_id + 1
+
+        teachers_ids = []
+        for emp_id in self.teachers.keys():
+            teachers_ids.append(int(emp_id[3:]))
+        highest_teacher_id = max(teachers_ids) if teachers_ids else 0
+        self.next_teacher_id = highest_teacher_id + 1
 
 system = ManagementSystem()
 system.load_data_from_file("data.json")
